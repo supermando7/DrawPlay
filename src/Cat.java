f25d5297974fe3c1683d7a8937824ab538131d2a
Armando Ocampo <aocampo@mines.edu>
2019-02-21 15:06:24 -0700

    lowered mouth off face

--- src/Cat.java
+++ src/Cat.java
@@ -18,7 +18,7 @@ public class Cat {
 	private static final int MOUTH_WIDTH = HEAD_DIMENSION/4;
 	// mouth starts about 40% from left edge of head
 	private static final int MOUTH_X = HEAD_DIMENSION/5 * 2;
-	private static final int MOUTH_Y = HEAD_DIMENSION/5 * 3;
+	private static final int MOUTH_Y = HEAD_DIMENSION/5 * 5;
 	
 	// draw will render the Cat on the Graphics object
 	public void draw(Graphics g, int catX, int catY)
